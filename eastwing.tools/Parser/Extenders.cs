59af71e5bccd2f05efaddd840e3c59bc300d98ce
Alexander Krasilnikov <krasilnikov@sharp-dev.net>
2017-03-16 23:17:19 +1000

    Реорганизация проекта

---
+++ eastwing.tools/Parser/Extenders.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Eastwing.Tools.Parser
+{
+    public static class Extenders
+    {
+        public static IEnumerable<Token> Parse(this string Source, Analyzer Parser)
+        {
+            return Parser.Parse(Source);
+        }
+
+        public static IEnumerable<Token> Parse(this string Source)
+        {
+            return Source.Parse(new Analyzer());
+        }
+    }
+}
--- eastwing.tools/Program.cs
+++ eastwing.tools/Program.cs
@@ -1,43 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Eastwing.Tools.Parser;
-using System.IO;
-using System.Diagnostics;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Eastwing.Tools.Parser;
+//using System.IO;
+//using System.Diagnostics;
 
-namespace Eastwing.Tools
-{
-    class Program
-    {
-        public static void Main(string[] args)
-        {
-            var parser = new Analyzer();
-            var str = File.ReadAllText("test.txt");
+//namespace Eastwing.Tools
+//{
+//    class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            var parser = new Analyzer();
+//            var str = File.ReadAllText("test.txt");
 
-            var s = new Stopwatch();
-            s.Start();
+//            var s = new Stopwatch();
+//            s.Start();
 
-            var p = new Analyzer()
-            {
-                Letters = "ФБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫБЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыбэюя",
-                Brackets = "",
-                Digits = ""
-            };
+//            var p = new Analyzer()
+//            {
+//                Letters = "ФБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫБЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыбэюя",
+//                Brackets = "",
+//                Digits = ""
+//            };
 
-            var result = str.Parse(p);
+//            var result = str.Parse(p);
 
-            Console.WriteLine(s.Elapsed.TotalMilliseconds);
-            s.Reset();
+//            Console.WriteLine(s.Elapsed.TotalMilliseconds);
+//            s.Reset();
 
-            Console.WriteLine(result.Count());
-            //foreach (var lexeme in result)
-            //{
-            //    Console.WriteLine(lexeme.Full);
-            //}
+//            Console.WriteLine(result.Count());
+//            //foreach (var lexeme in result)
+//            //{
+//            //    Console.WriteLine(lexeme.Full);
+//            //}
 
-            Console.Read();
-        }
-    }
-}
+//            Console.Read();
+//        }
+//    }
+//}
